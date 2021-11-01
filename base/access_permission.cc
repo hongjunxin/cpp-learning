4515562a89f35e1142c20eb2dfa20fab639ab68c
hongjunxin <3343420641@qq.com>
2021-11-01 17:49:14 +0800

    fix: access test

--- base/access_permission.cc
+++ base/access_permission.cc
@@ -2,22 +2,23 @@
 
 class Base {
 public:
-    void pub_fn() {};
-    int pub_mem;
+    void base_pub_fn() {};
+    int base_pub_mem;
 protected:
-    void prot_fn() {};
-    int prot_mem;
+    void base_prot_fn() {};
+    int base_prot_mem;
 private:
-    void priv_fn() {};
-    int priv_men;
+    void base_priv_fn() {};
+    int base_priv_mem;
 };
 
+
+/* publibc 继承测试 */
 class DrivedPublicInherited : public Base {
     friend void clobber(DrivedPublicInherited&);
     friend void clobber(Base&);
 public:
     void accessBaseDataTest();
-    void accessBaseFunTest();
 protected:
     int prot_mem_PublicInherited;
 private:
@@ -27,70 +28,108 @@ private:
 /* public 继承，子类只能访问基类中的 protected/public 成员 */
 void DrivedPublicInherited::accessBaseDataTest()
 {
-    //pri_mem = 0;  /* 错误！不能访问 */
-    prot_mem = 0;
-    pub_mem = 0;
-}
-void DrivedPublicInherited::accessBaseFunTest()
-{
-    //priv_fn();  /* 错误！不能访问 */
-    prot_fn();
-    pub_fn();
-}
+    //base_priv_mem = 0;  /* 错误！不能访问 */
+    base_prot_mem = 0;
+    base_pub_mem = 0;
 
-void accessProtectedMemberTest()
-{
-    DrivedPublicInherited d;
-
-    /* 错误！不能访问。外界不能访问类的 protected 成员 */
-    //d.prot_mem_PublicInherited = 0;
-
-    /* 错误！不能访问。子类内部能访问基类的 protected 成员。但外界不能通过
-       子类对象去访问基类的 protected 成员 */
-    //d.prot_mem = 0;  
+    //base_priv_fn();  /* 错误！不能访问 */
+    base_prot_fn();
+    base_pub_fn();    
 }
 
 /* 类对象中所有访问等级的成员，对于它的友元函数来说都是可以访问的 */
 void clobber(DrivedPublicInherited &s) { 
     s.priv_mem_PublicInherited = 0;
-    s.prot_mem = 0; 
+    s.base_prot_mem = 0; 
 }  
 
 /* 错误：clobber 不是 Base 的友元函数，所以不能访问 Base 的 protected/private 成员 */
 void clobber(Base &b) { 
-    //b.priv_mem = 0; 
-    //b.prot_mem = 0;
+    //b.base_priv_mem = 0; 
+    //b.base_prot_mem = 0;
 }  
 
 class D_DrivedPublicInherited : public DrivedPublicInherited {
 public:
-    void accessGrandpaProtectedMemberTest();
+    void accessBaseDataTest();
 };
 
-/* 子类可以访问爷爷类的 protected 成员 */
-void D_DrivedPublicInherited::accessGrandpaProtectedMemberTest()
+/* 父类 public 继承了爷爷类后，各个域的访问属性不变
+   所以孙类仍然可以访问爷爷类的 public/protected 成员
+*/
+void D_DrivedPublicInherited::accessBaseDataTest()
 {
-    prot_mem = 0;
+    //base_priv_mem = 0;  /* 错误！不能访问 */
+    base_pub_mem = 0;     /* 保持了 public 访问属性 */
+    base_prot_mem = 0;    /* 保持了 protected 访问属性 */
+
+    //base_priv_fn();  /* 错误！不能访问 */
+    base_pub_fn();
+    base_prot_fn();
 }
 
-class DerivedPrivateInherited : protected Base {
+void outAccessPublicInherited() {
+    DrivedPublicInherited d;
+    d.base_pub_fn();  /* public 继承后 public 域的访问属性不变，所以外界能访问 */
+    d.base_pub_mem = 0;
+}
+
+
+/* protected 继承测试 */
+class DerivedProtectedInherited : protected Base {
 public:
     void accessBaseDataTest();
-    void accessBaseFunTest();
+};
+
+/* protected 继承，子类只能访问基类中的 protected/public 成员 */
+void DerivedProtectedInherited::accessBaseDataTest()
+{
+    //base_priv_mem = 0;  /* 错误！不能访问 */
+    base_prot_mem = 0;
+    base_pub_mem = 0;
+
+    //base_priv_fn();  /* 错误！不能访问 */
+    base_prot_fn();
+    base_pub_fn();
+}
+
+/* 基类中的 public/protected 控制域被 protected 继承后，
+   在子类中都变成了 protected，所以外界不能访问
+*/
+void outAccessProtectedInherited()
+{
+    DerivedProtectedInherited d;
+    //d.base_pub_mem = 0;  /* 错误！不能访问 */
+}
+
+class D_DerivedProtectedInherited : public DerivedProtectedInherited {
+public:
+    void accessBaseDataTest();
+};
+
+void D_DerivedProtectedInherited::accessBaseDataTest()
+{
+    base_prot_mem = 0;  /* 保持了 protected 访问属性 */
+    base_pub_mem = 0;   /* 在 DerivedProtectedInherited 类中已经被转变为 protected 访问属性 */
+}
+
+
+/* private 继承测试 */
+class DerivedPrivateInherited : private Base {
+public:
+    void accessBaseDataTest();
 };
 
 /* private 继承，子类只能访问基类中的 protected/public 成员 */
 void DerivedPrivateInherited::accessBaseDataTest()
 {
-   // pri_mem = 0;  /* 错误！不能访问 */
-    prot_mem = 0;
-    pub_mem = 0;
-}
-void DerivedPrivateInherited::accessBaseFunTest()
-{
-   // priv_fn();  /* 错误！不能访问 */
-    prot_fn();
-    pub_fn();
+    //base_priv_mem = 0;  /* 错误！不能访问 */
+    base_prot_mem = 0;
+    base_pub_mem = 0;
+    
+    //base_priv_fn();  /* 错误！不能访问 */
+    base_prot_fn();
+    base_pub_fn();
 }
 
 class D_DerivedPrivateInherited : public DerivedPrivateInherited {
@@ -98,35 +137,37 @@ public:
     void accessBaseDataTest();
 };
 
-/* 虽然 DerivedPrivateInherited 私继承了 Base，但 
-   D_DerivedPrivateInherited 仍能访问 Base 的 protected/public 成员，
-   所以私继承限制的是类的使用者，而不是类继承体系中的子类 */
+/* private 继承后，基类中的 public/protected 域被强制转换为 private 访问属性,
+   所以不能被外界访问，也不能对于孙类访问，因为爷爷类中的 public/protected 域在父类
+   中已经被转变为 private 访问属性 */
 void D_DerivedPrivateInherited::accessBaseDataTest()
 {
-    prot_mem = 0;
-    pub_mem = 0;
+    // base_prot_mem = 0;  /* 错误！不能访问 */
+    // base_pub_mem = 0;   /* 错误！不能访问 */
+}
+void outAccessPrivateInherited() {
+    DerivedPrivateInherited d;
+    //d.base_pub_fn();  /* 错误！不能访问 */
 }
 
-class DerivedProtectedInherited : protected Base {
-
-};
-
-/* 对类的使用者来说，基类中的 public/protected 控制域，protected 继承后，在子类中都变成了 protected */
-void accessBaseMemberTest()
-{
-    DerivedProtectedInherited d;
-    //d.pub_mem = 0;  /* 错误！不能访问 */
-    //d.prot_mem = 0;  /* 错误！不能访问 */
-}
+/* 总结：
+   - 对于子类，不管是那种继承（public/protected/private），子类都可以访问基类中的
+     public/protected 成员，但是就是不能访问基类的 private 成员
+   - 对于外界，只能访问类的 public 成员
+   - public 继承后，基类中的 public/protected/private 对应的域访问属性不变
+   - protected 继承后，基类中的 public 域被强制转为 protected 访问属性
+   - private 继承后，基类中的 public/protected 域被强制转换为 private 访问属性
+ */
 
 int main()
 {
     DrivedPublicInherited d;
 
-    /* 子类对象不能向下转型为基类（不管是以引用、指针、还是非引用非指针的形式） */
-    //Base* pp = new DerivedPri;
-    //DerivedPri dp;
-    //Base b = dp;
+    Base* pp = new DrivedPublicInherited;
+    pp->base_pub_fn();
+
+    DrivedPublicInherited dp;
+    Base b = dp;
 
     return 0;
 }
